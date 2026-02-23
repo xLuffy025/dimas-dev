e0a5ce056cca3d39fcb28fd19ef3641316b9a64b
xLuffy025 <dimas.j0511@outlook.com>
2026-02-23 15:17:03 -0600

    update

--- experiments/bash/app_caja_proseso/caja_app/lib/msj.sh
+++ experiments/bash/app_caja_proseso/caja_app/lib/msj.sh
@@ -2,6 +2,17 @@
 set -eou pipefail
 IFS=$'\n\t'
 
+#----------------------------------------------------
+# Colores 
+#----------------------------------------------------
+RESET="\e[0m"
+ROJO="\e[31m"
+VERDE="\e[32m"
+AMARILLO="\e[33m"
+AZUL="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+BLANCO="\e[97m"
 # ---------------------------------------------------
 # Funciones de mensajes 
 # ---------------------------------------------------
@@ -10,4 +21,3 @@ ok(){ printf "%b [✅]%b %s\n" "$VERDE" "$RESET" "$1"; }
 warn(){ printf "%b[!]>%b %s\n" "$AMARILLO" "$RESET" "$1"; } 
 err(){ printf "%b[❌]%b %s\n" "$ROJO" "$RESET" "$1"; }
 
-msg "Error"
