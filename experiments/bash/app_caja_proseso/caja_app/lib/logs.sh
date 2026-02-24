3b0d184b8e2b395a0477fc0774c3c29ab3f5ffc1
xLuffy025 <dimas.j0511@outlook.com>
2026-02-24 12:25:39 -0600

    update logs,crear_socio,utils

--- experiments/bash/app_caja_proseso/caja_app/lib/logs.sh
+++ experiments/bash/app_caja_proseso/caja_app/lib/logs.sh
@@ -4,8 +4,29 @@ IFS=$'\n\t'
 
 log_info() { 
   local mensaje="$1"
-  if ! printf '[INFO] %S - %S\n' "$(date '+%F %T')" >> "$LOG_FILE"; then
-    warn "No se pudo escribir en el log: $LOG_FILE"
-  fi
+  if ! printf '[INFO] %s - %s\n' "$(date '+%F %T')" "$mensaje" >> "$LOG_FILE"; then
+    log_error "No se pudo escribir en el log: $LOG_FILE"
+  fi 
 
+}
+
+log_warn() {
+  local mensaje="$1"
+  if ! printf '[WARN] %s - %s\n' "$(date '+%F %T')" "$mensaje" >> "$LOG_FILE"; then
+    printf '[ERROR] No se pudo escribir en el log: %s\n' "$LOG_FILE" >&2
+  fi
+}
+
+log_error() {
+  local mensaje="$1"
+  if ! printf '[ERROR] %s - %s\n' "$(date '+%F %T')" "$mensaje" >> "$LOG_FILE"; then
+    printf '[ERROR] No se pudo escribir en el log: %s\n' "$LOG_FILE" >&2
+  fi
+}
+
+log_debug() {
+  local mensaje="$1"
+  if ! printf '[DEBUG] %s - %s\n' "$(date '+%F %T')" "$mensaje" >> "$LOG_FILE"; then
+    printf '[ERROR] No se pudo escribir en el log: %s\n' "$LOG_FILE" >&2
+  fi
 }
