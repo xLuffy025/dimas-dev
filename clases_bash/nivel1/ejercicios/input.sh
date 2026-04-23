76d34da0738b18f5a66bc33a1a65ccbcc56516a0
xLuffy025 <dimas.j0511@outlook.com>
2026-04-23 10:59:56 -0500

    clases_bash

---
+++ clases_bash/nivel1/ejercicios/input.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+read -p "Tu nombre: " nombre
+read -p "Tu edad: " edad
+read -p "Cual es tu Sistema favorito: " sistema
+
+echo "Nombre: $nombre"
+echo "Edad: $edad años"
+echo "Tu Sistema favorito es: $sistema"
+
