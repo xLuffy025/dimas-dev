47cdbbc544b6442648133d94afb28649f5a47091
xLuffy025 <dimas.j0511@outlook.com>
2026-05-06 10:53:52 -0500

    update

--- experiments/bash/pruebas/calculadora.sh
+++ experiments/bash/pruebas/calculadora.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
+while true; do
+  echo "Ingresa el primer numero:"
+  read NUM1
 
-echo "Ingresa el primer numero:"
-read NUM1
+  echo "Ingresa el Segundo numero:"
+  read NUM2
 
-echo "Ingresa el Segundo numero:"
-read NUM2
+  echo "Que operación desea reslizar? (+, - , *, /)"
 
-echo "Que operación desea reslizar? (+, - , *, /)"
+  read OPERACION
 
-read OPERACION
+  if [ "$OPERACION" = "+" ]; then 
+    RESULTADO=$((NUM1 + NUM2))
+  elif [ "$OPERACION" = "-" ]; then 
+    RESULTADO=$((NUM1 - NUM2))
+  elif [ "$OPERACION" = "*" ]; then
+    RESULTADO=$((NUM1 * NUM2))
+  elif [ "$OPERACION" = "/" ]; then
+    RESULTADO=$((NUM1 / NUM2))
+  else 
+    echo "Operación no valida"
+    return 
+  fi 
 
-if [ "$OPERACION" = "+" ]; then 
-  RESULTADO=$((NUM1 + NUM2))
-elif [ "$OPERACION" = "-" ]; then 
-  RESULTADO=$((NUM1 - NUM2))
-elif [ "$OPERACION" = "*" ]; then
-  RESULTADO=$((NUM1 * NUM2))
-elif [ "$OPERACION" = "/" ]; then
-  RESULTADO=$((NUM1 / NUM2))
-else
-  echo "Operación no valida"
-  exit 1 
-fi 
 echo "El resultado de $NUM1 $OPERACION $NUM2 es $RESULTADO"
+do 
+#  exit 1 
+
 
